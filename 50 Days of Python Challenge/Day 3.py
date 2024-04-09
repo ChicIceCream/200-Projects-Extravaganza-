3e6ed5b51994710231d16455c9e17c705aa2c251
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-04-09 15:02:24 +0530

    Day 3 finished

---
+++ 50 Days of Python Challenge/Day 3.py
@@ -0,0 +1,12 @@
+def register_check(register):
+    counter = 0
+    for key in register:
+        if register[key] == 'yes':
+            counter += 1
+    return counter
+
+register = {
+    'Michael':'yes','John': 'no',
+    'Peter':'yes', 'Mary': 'yes'
+            }
+print(f'The amount of students present are : {register_check(register)}')
