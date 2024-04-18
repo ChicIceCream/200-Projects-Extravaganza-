4ec6f186c5cbed81ac2e99f81fcc2593a5bd6cc9
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-04-18 19:27:13 +0530

    Day 10

--- 50 Days of Python Challenge/Day 10.py
+++ 50 Days of Python Challenge/Day 10.py
@@ -1,6 +1,6 @@
 def hide_password():
     actual_password = input("Give me your password : ")
     
-    return f' Your password is : {"*" * (len(actual_password) - 1)} and it is {len(actual_password) - 1} characters long'
+    return f' Your password is : {"*" * (len(actual_password) - 1)} and it is {(len(actual_password) - 1)} characters long'
 
 print(hide_password())
