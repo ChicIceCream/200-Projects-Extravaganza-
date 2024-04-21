714b2530e04f6a7f42156a05793c50a2c99c48a5
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-04-21 16:11:10 +0530

    Day 12

---
+++ 50 Days of Python Challenge/Day 12.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+def count_dots(string):
+    dot_count = 0
+    for char in string:
+        if char == '.':
+            dot_count += 1
+    
+    return dot_count
+
+print(count_dots("h.a.h.a."))
