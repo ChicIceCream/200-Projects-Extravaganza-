1e53c77ac1a9bb63122da2543c189e8aae3efb46
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-04-23 19:29:15 +0530

    Day 14

--- 50 Days of Python Challenge/Day 14.py
+++ 50 Days of Python Challenge/Day 14.py
@@ -1,7 +1,7 @@
 def flat_list(nested_list):
     final_list = []
     for list in nested_list:
-        final_list.expand(list)
+        final_list.extend(list)
     return final_list
 
-print(flat_list([[2,4,5,6]]))
+print(flat_list([[2,4,5,6],[3,5,8,76]]))
