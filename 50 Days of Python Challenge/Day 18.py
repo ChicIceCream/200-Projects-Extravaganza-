0e559220b00ce99ab5a0384d84edc4efcfc752c3
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-04-30 02:07:29 +0530

    Day 18

--- 50 Days of Python Challenge/Day 18.py
+++ 50 Days of Python Challenge/Day 18.py
@@ -8,3 +8,8 @@ def any_number(*args):
     return total / num_of_args
 
 print(any_number(12,90))
+'''
+I learned something new today! it is about the "*args". 
+I found out that we can input any number of inputs and 
+they are stored as a tuple!
+'''
