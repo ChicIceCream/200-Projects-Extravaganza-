2fef750e6628fc1dcedd1e07abb819372e3b0824
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-04-30 02:29:02 +0530

    Day 19

---
+++ 50 Days of Python Challenge/Day 19.py
@@ -0,0 +1,17 @@
+def count_words(string):
+    words = string.split()
+    
+    return len(words)
+
+def count_elements(string):
+    words = string.split()
+    
+    characters = 0
+    for letters in words:
+        characters += len(letters)
+    
+    return characters
+
+sentence = f"I love learning"
+print(f"Number of words: {count_words(sentence)}")  # Output: 3
+print(f"Number of elements:{count_elements(sentence)}")  # Output: 13
