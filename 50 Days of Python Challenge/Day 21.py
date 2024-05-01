393747bcce1ee44efc028cf2a80b08c94ba96521
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-05-01 23:29:24 +0530

    Day 21

--- 50 Days of Python Challenge/Day 21.py
+++ 50 Days of Python Challenge/Day 21.py
@@ -3,8 +3,8 @@ def make_tuples(list1, list2):
     if len(list1) != len(list2):
         return False
     
-    final_list = list1 + list2
-    
+    final_list = tuple(zip(list1, list2))
+
     return final_list
 
 print(make_tuples([1,2,3,4], [5,6,7,8]))
