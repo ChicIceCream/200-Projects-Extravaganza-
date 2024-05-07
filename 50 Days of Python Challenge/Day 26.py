5d03ec6d6bdb7adda9a301f065b8f9c09b7e3d10
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-05-07 21:56:56 +0530

    Day 26'

---
+++ 50 Days of Python Challenge/Day 26.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+def sort_words(string):
+    
+    hashmap = defaultdict(int)
+    
+    words = ''.join(string.split())
+    
+    for letter in words:
+        hashmap[letter] += 1
+    
+    letters = [x for x, count in hashmap.items() if count >= 1]
+    
+    return sorted(letters)
+
+
+print(sort_words('love life'))
