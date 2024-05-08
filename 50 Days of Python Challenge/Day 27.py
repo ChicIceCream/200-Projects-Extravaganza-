053f724255cd4a34d85da0a10cb432acc8f31d78
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-05-09 03:57:03 +0530

    Day 27

---
+++ 50 Days of Python Challenge/Day 27.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+
+def unique_numbers(numbers):
+    hashmap = defaultdict(int)
+    
+    for num in numbers:
+        hashmap[num] += 1
+    
+    unique_list = []
+    
+    for num in hashmap:
+        unique_list.append(num)
+    
+    # print(unique_list)
+    # print(numbers)
+    
+    if (sum(numbers) - sum(unique_list)) % 2 == 0:
+        # print(f"the sum of unique is:  {sum(unique_list)} and their difference is {sum(numbers) - sum(unique_list)}")
+        return numbers
+    else:
+        # print(f"the sum of unique is:  {sum(unique_list)} and their difference is {sum(numbers) - sum(unique_list)}")
+        return unique_list
+
+print(unique_numbers([1, 2, 4, 5, 6, 7, 8, 8]))
