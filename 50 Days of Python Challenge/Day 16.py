ac2a8517fcff9c9eb8414cdb57f48e197dcfaf49
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-06-13 22:35:04 +0530

    fixed

--- 50 Days of Python Challenge/Day 16.py
+++ 50 Days of Python Challenge/Day 16.py
@@ -1,14 +1,15 @@
 def sum_list(num_list):
     final_list = []
     for num in num_list:
-        final_list.extend(num)
+        for a in num:
+            final_list.append(a)
+    # total = 0
     
-    total = 0
+    # for num in final_list:
+    #     total += num
     
-    for num in final_list:
-        total += num
-    
-    return total
+    # return total
+    return final_list
 
 num_list = [[2, 4, 5, 6], [2, 3, 5, 6]]
 print(sum_list(num_list))
