eb9d54f1d6356a700c0ac92384bc344b80b0bd8a
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-08-04 22:30:59 +0530

    Day 32

--- 50 Days of Python Challenge/Day 32.py
+++ 50 Days of Python Challenge/Day 32.py
@@ -1,13 +1,17 @@
 def password_validator():
-    password = input("Enter your password: ")
-    
     while True:
-        if len(password) < 8: # longer than 8 letters
-            print("Password should be atleast 8 letters long")
-        elif not any(c.isupper() for c in password): # upper case letter
+        password = input("Enter your password: ")
+        
+        if len(password) < 8:  # longer than 8 letters
+            print("Password should be at least 8 letters long")
+        elif not any(c.isupper() for c in password):  # upper case letter
             print("Password should contain at least one uppercase letter")
-        elif not any(c.islower() for c in password): # lower case letter
+        elif not any(c.islower() for c in password):  # lower case letter
             print("Password should contain at least one lowercase letter")
-        elif not any(c.isdigit() for c in password): # atleast on digit
+        elif not any(c.isdigit() for c in password):  # at least one digit
             print("Password should contain at least one digit")
+        else:
+            print("Password is valid")
+            break
 
+password_validator()
