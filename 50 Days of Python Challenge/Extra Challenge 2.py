433a548fd2b0dd53760064fc3530daded0e8646e
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-08-22 19:24:39 +0530

    fixed

--- 50 Days of Python Challenge/Extra Challenge 2.py
+++ 50 Days of Python Challenge/Extra Challenge 2.py
@@ -1,5 +1,10 @@
-def your_salary(name, rate, rate_overtime, periods):
+def your_salary():
     
+    teacher_name = input("What is the teachers name? : ")
+    periods = int(input("How many periods have they taught this month? : "))
+    rate = int(input(f"What is {teacher_name}'s rate for each period they take? : "))
+    rate_overtime = int(input(f"What is their rate for overtime? : "))
+        
     if periods > 100:
         salary = (100 * rate) + ((periods - 100) * rate_overtime)
     else:
@@ -10,10 +15,4 @@ def your_salary(name, rate, rate_overtime, periods):
     return f'Teacher : {name} \nPeriods : {periods} \nGross Salary : {salary}'
 
 
-
-teacher_name = input("What is the teachers name? : ")
-period_number = int(input("How many periods have they taught this month? : "))
-rate = int(input(f"What is {teacher_name}'s rate for each period they take? : "))
-rate_overtime = int(input(f"What is their rate for overtime? : "))
-
-print(your_salary(teacher_name, rate, rate_overtime, period_number))
+print(your_salary())
