14a1ff55923cb1fa331035cf38492a0c5930e1c1
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-08-26 17:45:00 +0530

    bruteforced

---
+++ 50 Days of Python Challenge/Day 33.py
@@ -0,0 +1,10 @@
+def inter_section(list1, list2):
+    result = []
+    for num in list1:
+        for number in list2:
+            if num == number:
+                result.append(num)
+    
+    return result
+
+print(inter_section([20, 30, 60, 65, 75, 80, 85], [42, 30, 80, 65, 68, 88, 95]))
