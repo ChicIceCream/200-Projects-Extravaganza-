33016cff722b34e823c2f7c5a734ca377caeeed4
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-08-26 17:47:48 +0530

    using list comprehension

--- 50 Days of Python Challenge/Day 33.py
+++ 50 Days of Python Challenge/Day 33.py
@@ -7,4 +7,7 @@ def inter_section(list1, list2):
     
     return tuple(result)
 
+def inter_section(a, b):
+    return tuple(i for i in a if i in b)
+
 print(inter_section([20, 30, 60, 65, 75, 80, 85], [42, 30, 80, 65, 68, 88, 95]))
