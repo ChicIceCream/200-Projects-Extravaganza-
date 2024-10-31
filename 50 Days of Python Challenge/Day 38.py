d92713287c49a5222acc6988a225e877d2be4072
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-11-01 00:27:18 +0530

    commented and well

--- 50 Days of Python Challenge/Day 38.py
+++ 50 Days of Python Challenge/Day 38.py
@@ -1,19 +1,19 @@
-import random
-import time 
+import random # to get a random number
+import time # just for some fun
 
 def guess_a_number():
-    number_to_guess = random.randint(0,10)
+    number_to_guess = random.randint(0, 10)
     
     print("Welcome to Number Guessing!")
     time.sleep(1)
     
-    while True:
+    while True: # repeats forever until the condition is not true 
         number_guessed = int(input("Guess a number between 0 to 10 : "))
         if number_guessed > number_to_guess:
             print("Guess is too high! Try again!")
         elif number_guessed < number_to_guess:
             print("Guess is too low! Try Again!")
         else:
-            return "Correct! Well Done!"
+            return "Correct! Well Done!" # return breaks the loop
 
 print(guess_a_number())
