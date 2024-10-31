23e3a518e6e74569e8fb3e2a6d4e3c5e8847017e
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-11-01 00:02:10 +0530

    with explanation

--- 50 Days of Python Challenge/Day 37.py
+++ 50 Days of Python Challenge/Day 37.py
@@ -1,13 +1,13 @@
 from collections import defaultdict
 
 def count_the_vowels(input_string):
-    counter = defaultdict(int)
-    vowels = ["a", "e", "i", "o", "u"]
+    counter = defaultdict(int) # make a defaultdict for counter
+    vowels = ["a", "e", "i", "o", "u"] # make a list to check if the characters is one of thw vowel
     
     for char in input_string:
-        if char in vowels:    
+        if char in vowels:    # only include the character if it is a vowel
             counter[char] += 1
     
-    return len(counter.keys())    
+    return len(counter.keys())    # will return the length of the keys
 
 print(count_the_vowels("hello"))
