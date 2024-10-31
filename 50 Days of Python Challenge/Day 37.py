7df86a9bff18bf75f7bb643816d2e575206ba016
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-11-01 00:11:52 +0530

    added lower

--- 50 Days of Python Challenge/Day 37.py
+++ 50 Days of Python Challenge/Day 37.py
@@ -4,7 +4,7 @@ def count_the_vowels(input_string):
     counter = defaultdict(int) # make a defaultdict for counter
     vowels = ["a", "e", "i", "o", "u"] # make a list to check if the characters is one of thw vowel
     
-    for char in input_string:
+    for char in input_string.lower(): # to make sure it only counts the vowel once 
         if char in vowels:    # only include the character if it is a vowel
             counter[char] += 1
     
