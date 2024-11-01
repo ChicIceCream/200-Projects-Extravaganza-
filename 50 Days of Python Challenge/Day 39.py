3c9481ec6d2032d4301cd9e307c22c30cefbd706
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-11-01 21:30:00 +0530

    boom!

--- 50 Days of Python Challenge/Day 39.py
+++ 50 Days of Python Challenge/Day 39.py
@@ -2,25 +2,30 @@ import random
 import string
 
 def generate_password():
-    password = []
-    password_security = input('''Choose Your Security Level :
-            1 -> Low Security 
-            2 -> Strong Security
-            3 -> Very Strong Security
-    :''')
-    
-    if password_security == "1":
-        length = 5
-    elif password_security == "2":
-        length = 8
-    elif password_security == "3":
-        length = 12
-    else:
-        print("Wrong Input. Please try again!")
+    password = [] # make an empty list to store the password at the end
+    while True: # loops forever until the right answer is given
+        password_security = input('''Choose Your Security Level :
+                1 -> Low Security 
+                2 -> Strong Security
+                3 -> Very Strong Security
+        :''')
+        
+        if password_security == "1":
+            length = 5
+            break
+        elif password_security == "2":
+            length = 8
+            break
+        elif password_security == "3":
+            length = 12
+            break
+        else:
+            print("Wrong Input. Please try again!")
     
     for i in range(length):
-        password.append(random.choice(string.ascii_letters + string.digits + string.punctuation))
+        # use random and string modules to make sure a random letter, digit or punctuation is chosen
+        password.append(random.choice(string.ascii_letters + string.digits + string.punctuation)) 
     
-    return ''.join(password)
+    return f"Here is your generated password : {''.join(password)}"
 
 print(generate_password())
