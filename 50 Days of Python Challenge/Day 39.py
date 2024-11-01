67c79bcc7c5635e5ef7a07cd1bbabcb85027b4c6
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-11-01 21:12:37 +0530

    init

---
+++ 50 Days of Python Challenge/Day 39.py
@@ -0,0 +1,22 @@
+import random
+
+
+def generate_password():
+    password_security = input('''
+    Choose Your Security Level :
+        1 -> Low Security 
+        2 -> Strong Security
+        3 -> Very Strong Security
+    ''')
+    
+    if password_security == "1":
+        length = 5
+    elif password_security == "2":
+        length = 8
+    elif password_security == "3":
+        length = 12
+    else:
+        print("Wrong Input. Please try again!")
+    
+
+print(generate_password())
