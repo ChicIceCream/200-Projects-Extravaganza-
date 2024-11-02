e24b086545671f1dedd617089d024a5cb47b8ee6
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-11-02 20:43:28 +0530

    finished

--- 50 Days of Python Challenge/Day 40.py
+++ 50 Days of Python Challenge/Day 40.py
@@ -1,7 +1,13 @@
 def translate(input_string):
     final = []
     vowels = ["a", "e", "i", "o", "u"]
-    for word in list(input_string):
+    for word in list(input_string.split()):
         if word[0] in vowels:
             final.append(word + "yay")
+            print(final)
+        else:
+            final.append(word[1:] + word[0] + "ay")
+            print(final)
+    return " ".join(final)
 
+print(translate("i love python"))
