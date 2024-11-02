2be7d33fae8020bb1764945e901e2ebd2e82a9a9
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-11-02 20:45:51 +0530

    added comments and directions

--- 50 Days of Python Challenge/Day 40.py
+++ 50 Days of Python Challenge/Day 40.py
@@ -1,13 +1,14 @@
 def translate(input_string):
     final = []
-    vowels = ["a", "e", "i", "o", "u"]
-    for word in list(input_string.split()):
-        if word[0] in vowels:
+    vowels = ["a", "e", "i", "o", "u"] # added vowels to check if the word has vowels in it
+    
+    for word in list(input_string.split()): # split so every word is individual and list so we can iterate over the word
+        if word[0] in vowels: # if the first letter has a vowel
             final.append(word + "yay")
-            print(final)
-        else:
-            final.append(word[1:] + word[0] + "ay")
-            print(final)
-    return " ".join(final)
+        else: # if it doesnt
+            # iterates over the whole word after the first letter, adds the first letter and adds "ay" at the end
+            final.append(word[1:] + word[0] + "ay") 
+            
+    return " ".join(final) # join because the whole final variable is a list, not a sentence
 
 print(translate("i love python"))
