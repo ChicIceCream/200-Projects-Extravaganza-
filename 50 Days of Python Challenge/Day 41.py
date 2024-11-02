2ba93b9a6d0657a6f90189634dcad1a3a6e88481
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-11-02 23:54:46 +0530

    done

--- 50 Days of Python Challenge/Day 41.py
+++ 50 Days of Python Challenge/Day 41.py
@@ -1,12 +1,12 @@
 def words_with_vowels(input_string):
     final = []
-    vowels = ["a", "e", "i", "o", "u"]
+    vowels = ["a", "e", "i", "o", "u"] # make a vowel lsit to check if it exists in the word
     
-    for word in list(input_string.split()):
+    for word in list(input_string.split()): # list so we can iterate over every word
         for letter in word:
-            if letter in vowels:
+            if letter in vowels: # double for loop to check in letters
                 final.append(word)
-                break
+                break # break so it doesnt repeat for every vowel in the word 
     
     return final
 
