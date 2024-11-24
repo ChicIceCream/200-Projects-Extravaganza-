b05a1392940a364b9b9bcf34e7fa21c44eb2aee2
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-11-24 13:27:02 +0530

    module

--- OpenCV Projects/Virtual Drag & Drop/main.py
+++ OpenCV Projects/Virtual Drag & Drop/main.py
@@ -1,14 +1,15 @@
 import cv2 
 from cvzone.HandTrackingModule import HandDetector
+import HandTrackingModule as htm
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
-detector = HandDetector(detectionCon=0.8)
+detector = htm.handDetector(detectionCon=0.8)
 
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
-    lmList, _ = detector.findHands(img)
+    lmList = detector.findHands(img)
     cv2.imshow("Video Live", img)
     cv2.waitKey(1)
