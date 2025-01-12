689fa8f33c9d86cca64cda405258fdece13d9256
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2025-01-12 19:45:22 +0530

    lower model

--- Agentic AI/first try.py
+++ Agentic AI/first try.py
@@ -1,5 +1,5 @@
 from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
 
-agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=HfApiModel())
+agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=HfApiModel("meta-llama/Llama-3.2-3B-Instruct"))
 
 agent.run("How many seconds would it take for a leopard at full speed to run through Taj Mahal")
