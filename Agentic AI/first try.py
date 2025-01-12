079d8d2b87f99ffb86f1b69e875696262ddfb966
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2025-01-12 19:45:08 +0530

    oo

--- Agentic AI/first try.py
+++ Agentic AI/first try.py
@@ -2,4 +2,4 @@ from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
 
 agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=HfApiModel())
 
-agent.run("How much wood does a woodchuck chuck if a woodchuck could chuck wood?")
+agent.run("How many seconds would it take for a leopard at full speed to run through Taj Mahal")
