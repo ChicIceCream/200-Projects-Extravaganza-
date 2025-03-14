286740f6f3fa64dc1f3625e1c494e8a166ae8825
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2025-03-14 23:50:22 +0530

    trials

--- 50 Days of Python Challenge/Day 3.py
+++ 50 Days of Python Challenge/Day 3.py
@@ -1,9 +1,9 @@
-def register_check(register):
+def tally_affirmative_responses(register):
     counter = 0
     for key in register:
         if register[key] == 'yes':
             counter += 1
-    return counter
+    return counter + 
 
 register = {
     'Michael':'yes','John': 'no',
